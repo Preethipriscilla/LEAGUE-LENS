610268e5cc37553d67545bcc6af673c64a207de2
JIGLLYPUFF06 <preethipriscilla64@gmail.com>
2025-01-29 13:08:49 +0530

    Update leaguelensapp.py

--- leaguelensapp.py
+++ leaguelensapp.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-matplotlib.use('Agg')
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
